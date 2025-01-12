1e9ca1fabe9a74a33ba6097c43779ff4fcded0f3
Abhishek <abhishek@karveit.ca>
2025-01-12 01:42:09 -0500

    Host MongoDB in Mongo Atlas

--- app/config.py
+++ app/config.py
@@ -3,18 +3,17 @@ from typing import Any
 from pydantic_settings import BaseSettings
 from pymongo import MongoClient
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 class Config(BaseSettings):
     CORS_ORIGINS: list[str] = ["*"]
     CORS_HEADERS: list[str] = ["*"]
     CORS_METHODS: list[str] = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
 
-    MONGOHOST: str = "localhost"
-    MONGOPORT: str = "27017"
-    MONGOUSER: str = "root"
-    MONGOPASSWORD: str = "password"
     MONGODATABASE: str = "payment_database"
-    MONGO_URL: str = ""
+    MONGO_URL: str = os.getenv("MONGODB_URI")
 
 
 # environmental variables
@@ -25,9 +24,8 @@ fastapi_config: dict[str, Any] = {
     "title": "API",
 }
 
-mongo_url = (
-    f"mongodb://{env.MONGOUSER}:{env.MONGOPASSWORD}@{env.MONGOHOST}:{env.MONGOPORT}/"
-)
+mongo_url = ""
+
 if env.MONGO_URL:
     mongo_url = env.MONGO_URL
 
